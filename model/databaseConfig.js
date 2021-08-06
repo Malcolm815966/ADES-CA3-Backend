bfdc876da4d40a0085a5e933645431e9875f75c2
Malcolm815966 <76482311+Malcolm815966@users.noreply.github.com>
2021-08-06 10:22:00 +0800

    Changed pg require statement

--- model/databaseConfig.js
+++ model/databaseConfig.js
@@ -18,7 +18,7 @@
 //   }
 // };
 
-import pg from "pg";
+const pg = require('pg')
 const connectionString = `postgres://nntihaahbynyvl:e95b3d77ae6dbfeddbac6f37b0ce2c0ec0789b79692470c07c1a8d7b894270b6@ec2-52-0-67-144.compute-1.amazonaws.com:5432/d2snnmggr0fsrg`;
 
 const pool = new pg.Pool({

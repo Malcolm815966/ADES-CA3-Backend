4cdedf370f2181e73d6c67f81dac823e0764879c
Malcolm815966 <76482311+Malcolm815966@users.noreply.github.com>
2021-08-06 10:18:48 +0800

    Removed database.js, moved to databaseConfig

--- database.js
+++
@@ -1,8 +0,0 @@
-const connectionString = `postgres://nntihaahbynyvl:e95b3d77ae6dbfeddbac6f37b0ce2c0ec0789b79692470c07c1a8d7b894270b6@ec2-52-0-67-144.compute-1.amazonaws.com:5432/d2snnmggr0fsrg`;
-
-const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL || connectionString,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
